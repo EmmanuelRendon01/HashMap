5b6075668cefd2450339cfd8226a0fe6024dcbad
Emmanuel Rendon <116315044+EmmanuelRendon01@users.noreply.github.com>
2025-09-19 14:20:51 -0500

    Enhance comments on HashMap usage and definition
    Added explanation of HashMap and its functionality.

--- Explicacion/src/QueEs.java
+++ Explicacion/src/QueEs.java
@@ -10,6 +10,14 @@ public class QueEs {
         // Permite acceder rápidamente a los valores mediante una función hash.
         // Ejemplo: como una libreta de contactos (nombre → número).
 
+        // ¿Por qué se llama HashMap?
+
+        // Viene de la funcion hash, una operación matemática que toma una clave y la transforma en un número entero (un índice). Ese índice sirve para ubicar rápidamente dónde guardar o buscar el valor en memoria.
+        
+        // Map es la estrucura de datos que asocia pares clave-valor. Es decir, cada clave apunta a un valor.
+        
+        // Entonces, un HashMap es un Map que usa una función hash para organizar y acceder a sus elementos de forma muy rápida (normalmente en tiempo O(1)).
+
         HashMap<String, Integer> edades = new HashMap<>();
         edades.put("Ana", 20);
         edades.put("Luis", 25);
